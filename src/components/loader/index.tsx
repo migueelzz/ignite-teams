cea59c0b9bf33bd4d132073c9d6c804338df643d
migueelzz <miguellemes005@gmail.com>
2024-05-31 14:30:17 -0300

    feat: Add header component

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,10 @@
+import { ActivityIndicator } from 'react-native'
+import { Container, Loading } from './styles'
+
+export function Loader() {
+    return (
+        <Container>
+            <Loading />
+        </Container>
+    )
+}
